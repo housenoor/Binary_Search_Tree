891c0a3e61d9464586ea8ab37a5bb0a2686eb4c4
Srikanth <srikanthh994@gmail.com>
2021-10-24 18:18:56 +0530

    Added:Create A Binary Search Tree

---
+++ BinarySearchTree.java
@@ -0,0 +1,12 @@
+package com.binarytreesearch;
+
+public class BinarySearchTree {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Program");
+        BinaryTree<Integer> myTree = new BinaryTree<>();
+        myTree.add(56);
+        myTree.add(30);
+        myTree.add(70);
+        myTree.print(myTree.root);
+    }
+}
